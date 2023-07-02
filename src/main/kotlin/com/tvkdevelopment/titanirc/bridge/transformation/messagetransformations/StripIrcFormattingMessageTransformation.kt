ada8e4308e1319b7d5d5852b8881c7b7a9d0be16
tvkanters <tvkanters@gmail.com>
2023-07-02 22:29:01 +0200

    Strip mIRC colors

--- src/main/kotlin/com/tvkdevelopment/titanirc/bridge/transformation/messagetransformations/StripIrcFormattingMessageTransformation.kt
+++ src/main/kotlin/com/tvkdevelopment/titanirc/bridge/transformation/messagetransformations/StripIrcFormattingMessageTransformation.kt
@@ -1,13 +1,14 @@
 package com.tvkdevelopment.titanirc.bridge.transformation.messagetransformations
 
 import com.tvkdevelopment.titanirc.bridge.transformation.MessageTransformation
-import org.pircbotx.Colors
+import org.pircbotx.Colors.*
 
 class StripIrcFormattingMessageTransformation : MessageTransformation {
     override fun transform(message: String): String =
         message.replace(FORMATTING_REGEX, "")
 
     companion object {
-        private val FORMATTING_REGEX = Regex(Colors.LOOKUP_TABLE.values.joinToString("|"))
+        private val FORMATTING_REGEX = Regex("""$NORMAL|$BOLD|$ITALICS|$UNDERLINE|$REVERSE|\u0003\d,?\d""")
+
     }
 }
--- src/test/kotlin/com/tvkdevelopment/titanirc/bridge/transformation/messagetransformations/StripIrcFormattingMessageTransformationTest.kt
+++ src/test/kotlin/com/tvkdevelopment/titanirc/bridge/transformation/messagetransformations/StripIrcFormattingMessageTransformationTest.kt
@@ -9,9 +9,21 @@ class StripIrcFormattingMessageTransformationTest {
     private val sut = StripIrcFormattingMessageTransformation()
 
     @Test
-    fun testTransformation() {
+    fun testStripFormatting() {
         // GIVEN
         val message = "mes${Colors.BOLD}s${Colors.BLUE}a${Colors.NORMAL}ge"
+
+        // WHEN
+        val transformedMessage = sut.transform(message)
+
+        // THEN
+        assertEquals("message", transformedMessage)
+    }
+
+    @Test
+    fun testStripMircColors() {
+        // GIVEN
+        val message = "mes5,3sage"
 
         // WHEN
         val transformedMessage = sut.transform(message)
