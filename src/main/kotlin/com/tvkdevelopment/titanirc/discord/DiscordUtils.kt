aa48199f9c24e6c8b47e40bb0b4fe66c5ce54d15
tvkanters <tvkanters@gmail.com>
2023-07-06 21:35:55 +0200

    Decode username from bot messages in replies

--- src/main/kotlin/com/tvkdevelopment/titanirc/discord/DiscordUtils.kt
+++ src/main/kotlin/com/tvkdevelopment/titanirc/discord/DiscordUtils.kt
@@ -6,16 +6,32 @@ import dev.kord.core.entity.Message
 import dev.kord.core.entity.StickerItem
 import dev.kord.core.entity.channel.Channel
 import dev.kord.core.entity.effectiveName
+import kotlin.time.Duration.Companion.minutes
 
 val Channel?.topicValue: String
     get() = this?.data?.topic?.value ?: ""
 
 val Message.replyLabel: String
-    get() {
-        val authorName = author?.let { "${it.effectiveName} " }
-        val timeString = Time.getRelativeTimeString(Time.currentTime - timestamp, short = true)
-        return "[^$authorName$timeString]"
-    }
+    get() =
+        mutableListOf<String>()
+            .asSequence()
+            .plus(author?.takeIf { !it.isBot }?.effectiveName ?: bridgeNickname)
+            .plus(Time.getRelativeTimeString(Time.currentTime - timestamp, short = true, minimumTimePassed = 2.minutes))
+            .filterNot { it.isNullOrEmpty() }
+            .joinToString(" ")
+            .let { "[^$it]" }
+
+val Message.bridgeNickname: String?
+    get() =
+        BRIDGE_DISCORD_NICKNAME
+            .matchAt(content, 0)
+            ?.run {
+                groupValues
+                    .drop(1)
+                    .firstOrNull { it.isNotBlank() }
+            }
+
+private val BRIDGE_DISCORD_NICKNAME = Regex("""^(?:\*\*<([^>]+)>\*\*|\\\* ([^ ]+)) """)
 
 val StickerItem.label: String
     get() = "[$name sticker]"
--- src/main/kotlin/com/tvkdevelopment/titanirc/util/Time.kt
+++ src/main/kotlin/com/tvkdevelopment/titanirc/util/Time.kt
@@ -6,14 +6,16 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 import kotlin.time.DurationUnit
 
 object Time {
     val currentTime: Instant
         get() = Clock.System.now()
 
-    fun getRelativeTimeString(timePassed: Duration, short: Boolean): String =
+    fun getRelativeTimeString(timePassed: Duration, short: Boolean, minimumTimePassed: Duration = 0.seconds): String? =
         when {
+            timePassed < minimumTimePassed -> null
             timePassed >= 2.days -> "${timePassed.toInt(DurationUnit.DAYS)}${getDayString(plural = true, short)} ago"
             timePassed >= 1.days -> "1${getDayString(plural = false, short)} ago"
             timePassed >= 2.hours -> "${timePassed.toInt(DurationUnit.HOURS)}${getHourString(plural = true, short)} ago"
