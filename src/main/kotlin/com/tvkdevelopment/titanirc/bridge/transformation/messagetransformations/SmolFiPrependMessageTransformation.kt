fc05369b435c9d2d341c4a5089a70563198fb4f5
tvkanters <tvkanters@gmail.com>
2024-02-09 22:02:21 +0100

    Fixed smol.fi links not working due to lack of & encoding

--- src/main/kotlin/com/tvkdevelopment/titanirc/bridge/transformation/messagetransformations/SmolFiPrependMessageTransformation.kt
+++ src/main/kotlin/com/tvkdevelopment/titanirc/bridge/transformation/messagetransformations/SmolFiPrependMessageTransformation.kt
@@ -1,14 +1,17 @@
 package com.tvkdevelopment.titanirc.bridge.transformation.messagetransformations
 
 import com.tvkdevelopment.titanirc.bridge.transformation.MessageTransformation
+import java.net.URLEncoder
 
 class SmolFiPrependMessageTransformation : MessageTransformation {
 
     override fun transform(sourceChannel: String, targetChannel: String, message: String): String =
-        message.replace(REGEX, "https://smol.fi/e/?v=https://media.discordapp.net/attachments/$1")
+        message.replace(REGEX) { "https://smol.fi/e/?v=https://media.discordapp.net/attachments/${encode(it.groupValues[1])}" }
 
     companion object {
         private val REGEX =
-            Regex("""(?<!https://smol.fi/e/\?v=)https?://(?:media|cdn)\.discordapp\.(?:net|com)/attachments/(\S+\.(?:mp4|webm|mov))""")
+            Regex("""(?<!https://smol.fi/e/\?v=)https?://(?:media|cdn)\.discordapp\.(?:net|com)/attachments/(\S+\.(?:mp4|webm|mov)[^ ]*)""")
+
+        private fun encode(string: String) = URLEncoder.encode(string, Charsets.UTF_8.name())
     }
 }
